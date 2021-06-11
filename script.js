0fe87fa13c0d3c4f94e78769887f4b4b0e0eedbc
AsirIntesar <asirintesar97@gmail.com>
2021-06-11 22:02:16 +0600

    commit on master

--- script.js
+++ script.js
@@ -89,31 +89,4 @@ $(document).ready(function () {
       window.location.href = "certification/Data Scientist's Toolbox.pdf";
     });
   });    
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
